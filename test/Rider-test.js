ac58b3194c05b0160f760a7f4f2404f4fe93cc03
Seamus Quinn <sumaesquinn@gmail.com>
2018-03-21 16:49:36 -0600

    Add methods tests for rider class

--- test/Rider-test.js
+++ test/Rider-test.js
@@ -1,7 +1,6 @@
 const chai = require('chai');
 const assert = chai.assert;
 const Rider = require('../lib/Rider');
-// const context = canvas.getContext('2d');
 
 describe('Rider', function() {
   it('should be a function', function() {
@@ -42,5 +41,47 @@ describe('Rider', function() {
     var rider = new Rider(10, 12, 8, 8, 'red', 1);
     assert.equal(rider.xDir, 1)
   })
+
+  //Move Method
+
+  it('should be able to move on the x-axis', function() {
+    var rider = new Rider(10, 12, 8, 8, 'red', 1)
+
+    rider.move();
+
+    assert.equal(rider.x, 11);
+  })
+
+  it('should be able to move on the y-axis', function() {
+    var rider = new Rider(10, 12, 8, 8, 'red', 1)
+
+    rider.yDir = 1;
+
+    rider.move();
+
+    assert.equal(rider.y, 13);
+  })
+
+  //Change Direction Method
+
+  it('should be able to change it\'s x direction', function() {
+    var rider = new Rider(10, 12, 8, 8, 'red', 1)
+
+    assert.equal(rider.xDir, 1);
+
+    rider.changeDir(0, 1);
+
+    assert.equal(rider.xDir, 0);
+  }) 
+
+  it('should be able to change it\'s y direction', function () {
+    var rider = new Rider(10, 12, 8, 8, 'red', 1)
+
+    assert.equal(rider.yDir, 0);
+
+    rider.changeDir(0, 1);
+
+    assert.equal(rider.yDir, 1);
+  }) 
 
 })
