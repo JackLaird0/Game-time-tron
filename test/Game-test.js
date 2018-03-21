cb2e1ce511714e0761dfeee5770bae789159fbda
Seamus Quinn <sumaesquinn@gmail.com>
2018-03-21 17:29:04 -0600

    Add segement coordinates method testing

--- test/Game-test.js
+++ test/Game-test.js
@@ -2,7 +2,6 @@ const chai = require('chai');
 const assert = chai.assert;
 const Game = require('../lib/Game');
 const Rider = require('../lib/Rider');
-// const context = canvas.getContext('2d');
 
 describe('Game Tests', function() {
 
@@ -39,5 +38,74 @@ describe('Game Tests', function() {
     var game = new Game();
     assert.equal(game.gameState, 'start');
   })
+
+  //Segment Coordinates Method
+
+  it('when called, should return an object', function() {
+    var game = new Game();
+    
+    let coordinatesObject = game.segmentCoordinates();
+
+    assert.isObject(coordinatesObject);
+  })
+
+  it('should return an object with two players inside', function() {
+    var game = new Game();
+
+    let coordinatesObject = game.segmentCoordinates();
+
+    assert.equal(Object.keys(coordinatesObject).length, 2);
+  })
+
+  it('player one should have an x key', function() {
+    var game = new Game();
+
+    let coordinatesObject = game.segmentCoordinates();
+
+    let objectKeys = Object.keys(coordinatesObject)
+
+    let playerOneCoordinates = (coordinatesObject.playerOneCoordinates)
+    assert.equal(playerOneCoordinates.x, 92)
+    
+  })
+
+  it('player one should have an y key', function() {
+    var game = new Game();
+
+    let coordinatesObject = game.segmentCoordinates();
+
+    let objectKeys = Object.keys(coordinatesObject)
+
+    let playerOneCoordinates = (coordinatesObject.playerOneCoordinates)
+
+    assert.equal(playerOneCoordinates.y, 296)
+    
+  })
   
 })
+  it('player two should have an x key', function() {
+    var game = new Game();
+
+    let coordinatesObject = game.segmentCoordinates();
+
+    let objectKeys = Object.keys(coordinatesObject)
+
+    let playerTwoCoordinates = (coordinatesObject.playerTwoCoordinates)
+
+    assert.equal(playerTwoCoordinates.x, 692)
+    
+  })
+  
+  it('player two should have an y key', function() {
+    var game = new Game();
+
+    let coordinatesObject = game.segmentCoordinates();
+
+    let objectKeys = Object.keys(coordinatesObject)
+
+    let playerTwoCoordinates = (coordinatesObject.playerTwoCoordinates)
+
+    assert.equal(playerTwoCoordinates.y, 296)
+    
+  })
+  
