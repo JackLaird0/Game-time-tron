699bc1a1545362c7b6630de8f87f96050475aed9
Seamus Quinn <sumaesquinn@gmail.com>
2018-03-21 18:24:18 -0600

    Add game over method testing

--- test/Game-test.js
+++ test/Game-test.js
@@ -39,17 +39,19 @@ describe('Game Tests', function() {
     assert.equal(game.gameState, 'start');
   })
 
-  //Segment Coordinates Method
+})
 
-  it('when called, should return an object', function() {
+describe('Segment Coordinates Method', function(){
+
+  it('when called, should return an object', function () {
     var game = new Game();
-    
+
     let coordinatesObject = game.segmentCoordinates();
 
     assert.isObject(coordinatesObject);
   })
 
-  it('should return an object with two players inside', function() {
+  it('should return an object with two players inside', function () {
     var game = new Game();
 
     let coordinatesObject = game.segmentCoordinates();
@@ -57,7 +59,7 @@ describe('Game Tests', function() {
     assert.equal(Object.keys(coordinatesObject).length, 2);
   })
 
-  it('player one should have an x key', function() {
+  it('player one should have an x key', function () {
     var game = new Game();
 
     let coordinatesObject = game.segmentCoordinates();
@@ -66,10 +68,10 @@ describe('Game Tests', function() {
 
     let playerOneCoordinates = (coordinatesObject.playerOneCoordinates)
     assert.equal(playerOneCoordinates.x, 92)
-    
+
   })
 
-  it('player one should have an y key', function() {
+  it('player one should have an y key', function () {
     var game = new Game();
 
     let coordinatesObject = game.segmentCoordinates();
@@ -79,11 +81,10 @@ describe('Game Tests', function() {
     let playerOneCoordinates = (coordinatesObject.playerOneCoordinates)
 
     assert.equal(playerOneCoordinates.y, 296)
-    
+
   })
-  
-})
-  it('player two should have an x key', function() {
+
+  it('player two should have an x key', function () {
     var game = new Game();
 
     let coordinatesObject = game.segmentCoordinates();
@@ -93,10 +94,10 @@ describe('Game Tests', function() {
     let playerTwoCoordinates = (coordinatesObject.playerTwoCoordinates)
 
     assert.equal(playerTwoCoordinates.x, 692)
-    
+
   })
-  
-  it('player two should have an y key', function() {
+
+  it('player two should have an y key', function () {
     var game = new Game();
 
     let coordinatesObject = game.segmentCoordinates();
@@ -106,6 +107,245 @@ describe('Game Tests', function() {
     let playerTwoCoordinates = (coordinatesObject.playerTwoCoordinates)
 
     assert.equal(playerTwoCoordinates.y, 296)
-    
+
   })
+
+})
+
+describe('Game Over Method', function() {
+
+  describe('Wall Collision', function() {
+
+    it('game over should be a function', function () {
+      var game = new Game();
+      assert.isFunction(game.gameOver)
+    })
+
+    it('should check for player one top wall collision', function () {
+      var game = new Game();
+
+      game.playerOne.y = -9;
+
+      game.gameOver();
+
+      assert.equal(game.gameState, 'Player Two Wins')
+    })
+
+    it('should check for player one bottom wall collision', function () {
+      var game = new Game();
+
+      game.playerOne.y = 609;
+
+      game.gameOver();
+
+      assert.equal(game.gameState, 'Player Two Wins')
+    })
+
+    it('should check for player one left wall collision', function () {
+      var game = new Game();
+
+      game.playerOne.x = -9;
+
+      game.gameOver();
+
+      assert.equal(game.gameState, 'Player Two Wins')
+    })
+
+    it('should check for player one right wall collision', function () {
+      var game = new Game();
+
+      game.playerOne.x = 809;
+
+      game.gameOver();
+
+      assert.equal(game.gameState, 'Player Two Wins')
+    })
+
+    it('should check for player two top wall collision', function () {
+      var game = new Game();
+
+      game.playerTwo.y = -9;
+
+      game.gameOver();
+
+      assert.equal(game.gameState, 'Player One Wins')
+    })
+
+    it('should check for player two bottom wall collision', function () {
+      var game = new Game();
+
+      game.playerTwo.y = 609;
+
+      game.gameOver();
+
+      assert.equal(game.gameState, 'Player One Wins')
+    })
+
+    it('should check for player two left wall collision', function () {
+      var game = new Game();
+
+      game.playerTwo.x = -9;
+
+      game.gameOver();
+
+      assert.equal(game.gameState, 'Player One Wins')
+    })
+
+    it('should check for player two right wall collision', function () {
+      var game = new Game();
+
+      game.playerTwo.x = 809;
+
+      game.gameOver();
+
+      assert.equal(game.gameState, 'Player One Wins')
+    })
+  })
+
+  describe('Player to Player Collision', function() {
+
+    //Mock an array.
+    //Run gameOver method
+    //Assert that the correct player wins
+    
+    it('player one wins if player two hits player one', function() {
+      var game = new Game();
+
+      game.playerOneArray = [
+        {x: 32, y: 400},
+        {x: 47, y: 400},
+        {x: 109, y: 400}
+      ]
+      game.playerTwoArray = [
+        {x: 0, y: 0},
+        {x: 165, y: 280},
+        {x: 47, y: 400}
+      ]
+
+      game.gameOver();
+
+      assert.equal(game.gameState, 'Player One Wins');
+    })
+
+    it('player two wins if player one hits player two', function() {
+      var game = new Game();
+
+      game.playerTwoArray = [
+        {x: 32, y: 400},
+        {x: 47, y: 400},
+        {x: 109, y: 400}
+      ]
+      game.playerOneArray = [
+        {x: 0, y: 0},
+        {x: 165, y: 280},
+        {x: 47, y: 400}
+      ]
+
+      game.gameOver();
+
+      assert.equal(game.gameState, 'Player Two Wins');
+    })
+
+    it('should be a tie if they collide head on', function() {
+      var game = new Game();
+
+      game.playerTwoArray = [
+        {x: 32, y: 400},
+        {x: 109, y: 400},
+        {x: 47, y: 400}
+      ]
+      game.playerOneArray = [
+        {x: 0, y: 0},
+        {x: 165, y: 280},
+        {x: 47, y: 400}
+      ]
+
+      game.gameOver();
+
+      assert.equal(game.gameState, 'tied');
+    })
+  })
+
+  describe('Player One Self Collision', function() {
+    it('player two should win if player one collides with itself', function() {
+      var game = new Game();
+
+      game.playerOneArray = [
+        { x: 32, y: 400 },
+        { x: 47, y: 400 },
+        { x: 32, y: 400 },
+        { x: 32, y: 400 },
+        { x: 47, y: 400 },
+        { x: 32, y: 400 },
+        { x: 32, y: 400 },
+        { x: 47, y: 400 },
+        { x: 32, y: 400 },
+        { x: 32, y: 400 },
+        { x: 47, y: 400 },
+        { x: 32, y: 400 },
+        { x: 32, y: 400 },
+        { x: 47, y: 400 },
+        { x: 32, y: 400 },
+        { x: 32, y: 400 },
+        { x: 47, y: 400 },
+        { x: 32, y: 400 },
+        { x: 32, y: 400 },
+        { x: 47, y: 400 },
+        { x: 32, y: 400 },
+        { x: 32, y: 400 },
+        { x: 47, y: 400 },
+        { x: 32, y: 400 }
+      ]
+
+      game.gameOver();
+
+      assert.equal(game.gameState, 'Player Two Wins')
+    })
+
+    it('player one should win if player two collides with itself', function() {
+      var game = new Game();
+
+      game.playerTwoArray = [
+        { x: 32, y: 400 },
+        { x: 47, y: 400 },
+        { x: 32, y: 400 },
+        { x: 32, y: 400 },
+        { x: 47, y: 400 },
+        { x: 32, y: 400 },
+        { x: 32, y: 400 },
+        { x: 47, y: 400 },
+        { x: 32, y: 400 },
+        { x: 32, y: 400 },
+        { x: 47, y: 400 },
+        { x: 32, y: 400 },
+        { x: 32, y: 400 },
+        { x: 47, y: 400 },
+        { x: 32, y: 400 },
+        { x: 32, y: 400 },
+        { x: 47, y: 400 },
+        { x: 32, y: 400 },
+        { x: 32, y: 400 },
+        { x: 47, y: 400 },
+        { x: 32, y: 400 },
+        { x: 32, y: 400 },
+        { x: 47, y: 400 },
+        { x: 32, y: 400 }
+      ]
+
+      game.gameOver();
+
+      assert.equal(game.gameState, 'Player One Wins')
+    })
+  })
+
+  describe('Player Two Self Collision', function() {
+
+  })
+
+})
+
   
+
+ 
+  
+
